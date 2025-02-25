7845e867bfe7008904a168b92dcb091857d99f32
xfelix <xavierfelix74@gmail.com>
2025-02-25 15:30:21 -0800

    current test build

---
+++ tests/cache/cache_test.go
@@ -0,0 +1,13 @@
+package cache_test
+
+import (
+	"testing"
+	// "github.com/XaviFelix/go-cache-server.git/cmd/cache"
+)
+
+// Needs to use every method in this test
+// Add logging
+func TestLRUCache(t *testing.T) {
+	// c := cache.New[string, string]()
+	// finish
+}
