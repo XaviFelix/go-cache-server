a9eb586dc6f3b02c5f032c7f26ccb8131d1fb418
xfelix <xavierfelix74@gmail.com>
2025-02-25 23:31:00 -0800

    testing current cache build

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -2,9 +2,19 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/XaviFelix/go-cache-server.git/cmd/cache"
 )
 
 func main() {
-	fmt.Println("Testing..")
+	lruCache := cache.New[string, string]()
+
+	lruCache.Put("hello", "world")
+	lruCache.Put("Xavier", "xavi")
+
+	lruCache.Put("Hi", "there")
+	lruCache.Put("fish", "shark")
+
+	fmt.Println(lruCache.GetSize())
 
 }
