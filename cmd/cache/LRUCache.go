1b14ead7293a60a178fe6c0ef3d8f3617038b6c2
xfelix <xavierfelix74@gmail.com>
2025-02-25 23:33:05 -0800

    updated cache build

--- cmd/cache/LRUCache.go
+++ cmd/cache/LRUCache.go
@@ -5,10 +5,11 @@ import (
 )
 
 // TODO: Put() and Evict(), then test them
+// TODO: Add more functionality to this interface
 type CacheADT[K comparable, V any] interface {
 	Get(key K) (V, bool)
 	Put(key K, value V)
-	Evict(key K)
+	Evict() (V, bool)
 }
 
 type CacheEntry[V any] struct {
@@ -17,14 +18,18 @@ type CacheEntry[V any] struct {
 }
 
 type LRUCache[K comparable, V any] struct {
-	items map[K]CacheEntry[V]
-	list  *list.List
+	items    map[K]CacheEntry[V]
+	list     *list.List
+	size     int
+	capacity int
 }
 
 func New[K comparable, V any]() *LRUCache[K, V] {
 	return &LRUCache[K, V]{
-		items: make(map[K]CacheEntry[V]),
-		list:  list.New(),
+		items:    make(map[K]CacheEntry[V]),
+		list:     list.New(),
+		size:     0,
+		capacity: 2,
 	}
 }
 
@@ -40,9 +45,40 @@ func (c *LRUCache[K, V]) Get(key K) (V, bool) {
 	return item.value, true
 }
 
-// TODO: Finish this
 func (c *LRUCache[K, V]) Put(key K, value V) {
-	// if key exists then update its value and move it to the front (MRU)
-	// else if key doesn't exist, create a new entry and add it to map and list (push to front on list for LRU)
+	if item, exists := c.items[key]; exists {
+		item.value = value
+		c.list.MoveToFront(item.element)
+	} else {
+		if c.size >= c.capacity {
+			c.Evict()
+		}
 
+		element := c.list.PushFront(key)
+		c.items[key] = CacheEntry[V]{value, element}
+		c.size++
+	}
+}
+
+// THis removes the least recently used (LRU)
+func (c *LRUCache[K, V]) Evict() (V, bool) {
+	e := c.list.Back()
+	if e == nil {
+		var zero V
+		return zero, false
+	}
+
+	key := e.Value.(K)
+	c.list.Remove(e)
+
+	entry := c.items[key]
+	delete(c.items, key)
+
+	c.size--
+
+	return entry.value, true
+}
+
+func (c *LRUCache[K, V]) GetSize() int {
+	return c.size
 }
