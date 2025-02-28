77c05f45687c4a203ae23a6d8e822ac715da1580
xfelix <xavierfelix74@gmail.com>
2025-02-27 22:20:03 -0800

    testing cache capacity

--- tests/cache/cache_test.go
+++ tests/cache/cache_test.go
@@ -9,29 +9,40 @@ import (
 func TestLRUCache(t *testing.T) {
 	// Intance of LRUCache
 	t.Log("Testing LRU Cache")
-	c := cache.New[string, string]()
+	c := cache.New[string, string](2)
 
 	// Testing Put and Get method
 	c.Put("sport1", "boxing")
 	c.Put("sport2", "basketball")
-	item, exist := c.Get("sport2")
+	item, exist := c.Get("sport1")
 
 	if !exist {
-		t.Errorf("Expected an existing Value (basketball), returned: %t", exist)
+		t.Errorf("Expected an existing Value (boxing), returned: %t", exist)
 	} else {
-		t.Logf("Value of sport2: %v\n", item)
+		t.Logf("Value of sport1 (boxing): %v\n", item)
 	}
 
 	// Testing Evict()
 	lru, exist := c.Evict()
 	if !exist {
-		t.Errorf("Expected a least recently used item (boxing), instead got %t", exist)
+		t.Errorf("Expected a least recently used item (basketball), instead got %t", exist)
 	} else {
-		t.Logf("LRU result (boxing) in default fromat: %v\n", lru)
-		t.Logf("LRU result's (boxing) field names: %+v\n", lru) // this format is used for structs
-		t.Logf("LRU result (boxing) with Go-syntax representation: %#v\n", lru)
+		t.Logf("LRU result (basketball) in default fromat: %v\n", lru)
+		t.Logf("LRU result's (basketball) field names: %+v\n", lru) // this format is used for structs
+		t.Logf("LRU result (basketball) with Go-syntax representation: %#v\n", lru)
+		t.Logf("The size of the cache is now 1: %d", c.GetSize())
 	}
 
 	// TODO: Test when size reaches capacity
+	c.Put("sport3", "swimming")
+	c.Put("sport4", "hokey")
 
+	// Checking if LRU still exists in the cache:
+	// boxing should be gone and the order should be (LRU) swimming -> hokey(MRU)
+	leastRecently, _ := c.Evict()
+	mostRecently, _ := c.Evict()
+
+	t.Logf("LRU should be swimming: %v\n", leastRecently)
+	t.Logf("MRU should be hockey: %v\n", mostRecently)
+	t.Logf("Size of the cache should be zero: %d\n", c.GetSize())
 }
